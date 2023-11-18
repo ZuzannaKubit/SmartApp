b24658c6087a14a96eb2ea21352598d16c8d6aed
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:02:35 +0100

    add RegisterScreen to roots

--- app/src/main/java/com/zuzob00l/smartapp/navigation/Screens.kt
+++ app/src/main/java/com/zuzob00l/smartapp/navigation/Screens.kt
@@ -3,12 +3,14 @@ package com.zuzob00l.smartapp.navigation
 enum class SmartAppScreens() {
     StartScreen,
     LoginScreen,
-    HomeScreen;
+    HomeScreen,
+    RegisterScreen;
 
     companion object {
         fun fromRoute(route: String?): SmartAppScreens = when (route?.substringBefore("/")) {
             StartScreen.name -> StartScreen
             LoginScreen.name -> LoginScreen
+            RegisterScreen.name -> RegisterScreen
             HomeScreen.name -> HomeScreen
             null -> HomeScreen
             else -> throw IllegalArgumentException("route $route is not recognized")
