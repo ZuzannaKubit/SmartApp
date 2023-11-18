f0da4011a9d8a998e13d1dfd89a7fc61d7c7ce6a
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:22:31 +0100

    Create SignInState.kt
    class , that holds actual state of UI while login and Register

---
+++ app/src/main/java/com/zuzob00l/smartapp/screens/loginScreen/SignInState.kt
@@ -0,0 +1,7 @@
+package com.zuzob00l.smartapp.screens.loginScreen
+
+data class SignInState(
+    val isLoading: Boolean = false,
+    val isSuccess: String? = "",
+    val isError: String? =  ""
+)
