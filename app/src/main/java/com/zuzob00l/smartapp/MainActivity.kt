b27b279944db0cb85c60135e603e589546f777f7
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:13:16 +0100

    add SmartApp() to onCreate

--- app/src/main/java/com/zuzob00l/smartapp/MainActivity.kt
+++ app/src/main/java/com/zuzob00l/smartapp/MainActivity.kt
@@ -3,45 +3,48 @@
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.zuzob00l.smartapp.navigation.SmartAppNavigation
+import com.zuzob00l.smartapp.screens.loginScreen.SignInViewModel
 import com.zuzob00l.smartapp.ui.theme.SmartAppTheme
 import dagger.hilt.android.AndroidEntryPoint
  @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+     private val SignInViewModel: SignInViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SmartAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+                SmartApp()
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartAppTheme {
-        Greeting("Android")
-    }
-}
+ @Composable
+ fun SmartApp()
+ {
+     Surface(
+         modifier = Modifier.fillMaxSize(),
+         color = MaterialTheme.colorScheme.background
+     ) {
+         Column(
+             verticalArrangement = Arrangement.Center,
+             horizontalAlignment = Alignment.CenterHorizontally)
+         {
+             SmartAppNavigation()
+         }
+     }
+ }
