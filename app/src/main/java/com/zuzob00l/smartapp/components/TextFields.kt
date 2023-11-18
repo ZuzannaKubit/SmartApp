fc721ca574610467fdc73de4b3dfdbc8d230c108
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:00:37 +0100

    Update TextFields.kt
    change values type in all textFields

--- app/src/main/java/com/zuzob00l/smartapp/components/TextFields.kt
+++ app/src/main/java/com/zuzob00l/smartapp/components/TextFields.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.IconButton
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -29,44 +31,50 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zuzob00l.smartapp.R
+import com.zuzob00l.smartapp.modifiers.textFieldsModifier
 
 @Composable
 fun EmailTextField(
     modifier : Modifier = Modifier,
-    email: String,
+    emailState: MutableState<String>,
     enabled: Boolean = true,
-    singleLine: Boolean = true,
-    onNewEmail: (String) -> Unit)
+    singleLine: Boolean = true)
 {
      OutlinedTextField(
-         value = email,
-         onValueChange = { onNewEmail(it) },
+         modifier = Modifier.textFieldsModifier(),
+         value = emailState.value,
+         onValueChange = { emailState.value = it },
          leadingIcon = { Icon(
              imageVector = Icons.Default.Email,
              contentDescription = "email") },
-         placeholder = {Text("Enter Your Email Here...") },
+         textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
+         placeholder = {Text("Enter Your Email Here...", color = Color.Gray) },
          shape = RoundedCornerShape(10),
          colors = TextFieldDefaults.outlinedTextFieldColors(
              focusedBorderColor = Color.Yellow,
-             leadingIconColor = Color.White,
+             leadingIconColor = Color.Gray,
              backgroundColor = Color.Transparent)
      )
 }
 @Composable
 fun PasswordField(
     modifier: Modifier = Modifier,
-    password: String,
-    onPasswordChange: (String) -> Unit)
+    placeholder: String = "password",
+    passwordState: MutableState<String>)
 {
-   PasswordTextField(password = password, onPasswordChange = onPasswordChange, modifier = modifier)
+   PasswordTextField(
+       passwordState = passwordState,
+       placeholder = placeholder,
+       modifier = Modifier.textFieldsModifier())
 }
 
 @Composable
 private fun PasswordTextField(
     modifier: Modifier = Modifier,
-    password: String,
-    onPasswordChange: (String) -> Unit)
+    passwordState: MutableState<String>,
+    placeholder: String)
 {
     var visibility by remember { mutableStateOf(false) }
     val passwordIcon = if(visibility) painterResource(R.drawable.ic_visibility_off)
@@ -75,9 +83,10 @@ private fun PasswordTextField(
     val visualTransformation = if(visibility) VisualTransformation.None else PasswordVisualTransformation()
 
     OutlinedTextField(
-        value = password,
-        onValueChange = { onPasswordChange(it) },
-        placeholder = { Text(text = "password") },
+        modifier = modifier,
+        value = passwordState.value,
+        onValueChange = { passwordState.value = it },
+        placeholder = { Text(text = placeholder) },
         leadingIcon = { Icon(imageVector = Icons.Filled.Lock, contentDescription = "password leadingIcon") },
         trailingIcon = {
             IconButton(onClick = { visibility = !visibility }) {
@@ -85,14 +94,40 @@ private fun PasswordTextField(
                     painter = passwordIcon,
                     contentDescription = "password trailing icon")
         }},
+        textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Password),
         visualTransformation = visualTransformation,
         shape = RoundedCornerShape(10),
         colors = TextFieldDefaults.outlinedTextFieldColors(
             focusedBorderColor = Color.Yellow,
-            leadingIconColor = Color.White,
+            leadingIconColor = Color.Gray,
             backgroundColor = Color.Transparent)
+    )
+}
+@Composable
+fun UserDataTextField(
+    modifier: Modifier = Modifier,
+    userDataState: MutableState<String>,
+    singleLine: Boolean = true,
+    enabled: Boolean = true,
+    placeholder: String)
+{
+    OutlinedTextField(
+        modifier = Modifier.textFieldsModifier(),
+        value = userDataState.value,
+        onValueChange = {userDataState.value = it},
+        leadingIcon = { Icon(
+            imageVector = Icons.Filled.Person,
+            contentDescription = null)},
+        textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
+        placeholder = { Text(text = placeholder, color = Color.Gray) },
+        shape = RoundedCornerShape(10),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            backgroundColor = Color.Transparent,
+            focusedBorderColor = Color.Yellow,
+            leadingIconColor = Color.Gray
+        )
     )
 }
 
