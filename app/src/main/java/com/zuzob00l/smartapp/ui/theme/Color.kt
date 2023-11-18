ee1699846eead52850a29144a3de38c75076e2f2
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 13:58:20 +0100

    add customised colors

--- app/src/main/java/com/zuzob00l/smartapp/ui/theme/Color.kt
+++ app/src/main/java/com/zuzob00l/smartapp/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.zuzob00l.smartapp.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundGray = Color(0xFFD6D6D6)
+val YellowLight = Color(0xFFFFEE32)
+val YellowDark = Color(0xFFFFD100)
+val GrayLight = Color(0xFF333533)
+val GrayDark = Color(0xFF202020)
