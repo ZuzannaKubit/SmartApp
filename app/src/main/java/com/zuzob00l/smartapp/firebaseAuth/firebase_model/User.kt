bc6c90966dfff1436a4c30dc9a8208b744ad236e
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:01:33 +0100

    create new User class compatibile with firebase

---
+++ app/src/main/java/com/zuzob00l/smartapp/firebaseAuth/firebase_model/User.kt
@@ -0,0 +1,10 @@
+package com.zuzob00l.smartapp.firebaseAuth.firebase_model
+
+data class User(
+    val name: String? = null,
+    val surname: String? = null,
+    val image: String? = null,
+    val uid: String? = null,
+    val email: String? = null
+
+)
