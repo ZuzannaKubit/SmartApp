8c430e9220cd70a726078e997a0bc7d680ea21a0
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 14:02:02 +0100

    create new membership class

---
+++ app/src/main/java/com/zuzob00l/smartapp/firebaseAuth/firebase_model/Membership.kt
@@ -0,0 +1,12 @@
+package com.zuzob00l.smartapp.firebaseAuth.firebase_model
+
+data class Membership(
+    val type: String? = null,
+    val price: String? = null,
+    val duration: String? = null
+)
+
+
+
+
+
