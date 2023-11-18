fb6436157c54465380e444224ced69cdf6419c09
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-18 13:58:42 +0100

    Delete User.kt

--- app/src/main/java/com/zuzob00l/smartapp/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.zuzob00l.smartapp.model
-
-data class User(
-    val id: String?,
-    val userId: String,
-    val isMembership: Boolean,
-    val userGym: String,
-    val membership: Membership
-)
