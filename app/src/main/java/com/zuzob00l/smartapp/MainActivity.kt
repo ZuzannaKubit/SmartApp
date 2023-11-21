54774ddc649fe5a12a1782e21289a64e33c40c53
ZuzannaKubit <56255265+ZuzannaKubit@users.noreply.github.com>
2023-11-21 12:02:20 +0100

    call the new navigation from the main

--- app/src/main/java/com/zuzob00l/smartapp/MainActivity.kt
+++ app/src/main/java/com/zuzob00l/smartapp/MainActivity.kt
@@ -9,18 +9,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.zuzob00l.smartapp.navigation.SmartAppNavigation
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.zuzob00l.smartapp.navigation.nav_graph.SetupNavGraph
 import com.zuzob00l.smartapp.screens.loginScreen.SignInViewModel
 import com.zuzob00l.smartapp.ui.theme.SmartAppTheme
 import dagger.hilt.android.AndroidEntryPoint
  @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+     private lateinit var navController: NavHostController
      private val SignInViewModel: SignInViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +29,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             SmartAppTheme {
 
-                SmartApp()
+                navController = rememberNavController()
+                SmartApp(navController)
             }
         }
     }
 }
  @Composable
- fun SmartApp()
+ fun SmartApp(navController: NavHostController)
  {
      Surface(
          modifier = Modifier.fillMaxSize(),
@@ -44,7 +46,7 @@ class MainActivity : ComponentActivity() {
              verticalArrangement = Arrangement.Center,
              horizontalAlignment = Alignment.CenterHorizontally)
          {
-             SmartAppNavigation()
+             SetupNavGraph(navController = navController)
          }
      }
  }
